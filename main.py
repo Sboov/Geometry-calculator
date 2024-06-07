a428b3fff24e6e570fb752c57492ca1637064231
Sboov <patryk.padol@gmail.com>
2024-06-07 19:30:45 +0200

    0.2v
    Added classes and funtions to more readable code

--- main.py
+++ main.py
@@ -1,76 +1,96 @@
 import math
+class Square:
+    def __init__(self, side_lenght):
+        if side_lenght <=0:
+            raise ValueError("Side lenght must be a positive number")
+        self.side_lenght = side_lenght
+    
+    def calculate_permiter(self):
+        return self.side_lenght *4
+    def calculate_area(self):
+        return self.side_lenght^2
+class Rectangle:
+    def __init__(self, side_lenghtA, side_lenghtB):
+        if side_lenghtA <= 0 or side_lenghtB <= 0:
+            raise ValueError("Side lenght must be a positive number")
+        self.side_lenghtA = side_lenghtA
+        self.side_lenghtB = side_lenghtB
+
+    def calculate_permiter(self):
+        return (self.side_lenghtB *2 ) +(self.side_lenghtA * 2)
+    def calculate_area(self):
+        return self.side_lenghtA * self.side_lenghtB
+class Triangle_equilateral:
+    def __init__ (self, side_lenght):
+        if side_lenght <=0:
+            raise ValueError("Side lenght must be a positive number")
+        self.side_lenght = side_lenght
+    def calculate_permiter(self):
+        return self.side_lenght *3
+    def calculate_area(self):
+        return (math.sqrt(3) / 2) * self.side_lenght
 
 while True:
     print("Main menu: ")
     print("1. Square")
     print("2. Rectangle")
     print("3. Equilateral triangle")
-    print("4. Exit")  
-    choose = input("Choose your geometry figure: ")
-
-    if choose == '1':
-        while True:  
-            print("Choose your action:")
+    print("4. Exit")
+    choose = input("Choose your Action: ")
+    if choose == "1":
+        while True:
             print("1. Circuit calculations")
             print("2. Field calculations")
-            print("3. Back to main menu")
-            action = input("Choose your action: ")
-            if action == '1':
-                a = int(input("Type the side length: "))
-                square_circut = a * 4
-                print("Square circuit is: ", square_circut)
-            elif action == '2':
-                a = int(input("Type the side length: "))
-                square_field = a ** 2
-                print("Square field is: ", square_field)
-            elif action == '3':
-                break  
-            else:
-                print("Wrong action. Try again!")
-                
-    elif choose == '2':
-        while True: 
-            print("Choose your action:")
-            print("1. Circuit calculations")
-            print("2. Field calculations")
-            print("3. Back to main menu")
             action = input("Choose your action: ")
             if action == "1":
-                a = int(input("Type the first side length: "))
-                b = int(input("Type the second side length: "))
-                rectangle_circuit = (2 * a) + (2 * b)
-                print("Rectangle circuit is: ", rectangle_circuit)
+                side_lenght = float(input("Enter the side lenght: "))
+                square = Square(side_lenght)
+                print("Perimeter of the square:", square.calculate_permiter())
             elif action == "2":
-                a = int(input("Type the first side length: "))
-                b = int(input("Type the second side length: "))
-                rectangle_field = a * b
-                print("Rectangle field is: ", rectangle_field)
-            elif action == "3":
-                break  
+                side_lenght = float(input("Enter the side lenght: "))
+                square = Square(side_lenght)
+                print("Area of the square: ", square.calculate_area())
             else:
-                print("Wrong action. Try again!")
-                
-    elif choose == '3':
-        while True:  
-            print("Choose your action:")
+                print("Error. Try Again! ")
+                continue
+            break
+    elif choose == "2":
+        while True:
             print("1. Circuit calculations")
             print("2. Field calculations")
-            print("3. Back to main menu")
-            action = input("Choose your action: ")
+            action = input("Chose your action: ")
             if action == "1":
-                a = int(input("Type the side length: "))
-                equilateral_triangle = a * 3
-                print("Circuit of equilateral triangle is: ", equilateral_triangle)
+                side_lenghtA = float(input("Enter the side A lenght: "))
+                side_lenghtB = float(input("Enter the side B lenght: "))
+                rectangle = Rectangle(side_lenghtA, side_lenghtB)
+                print ("Permieter of rectangle is: ", rectangle.calculate_permiter())
             elif action == "2":
-                a = int(input("Type the side length: "))
-                equilateral_triangle_field = (a ** 2 * math.sqrt(3)) / 4
-                print("Field of equilateral triangle is: ", equilateral_triangle_field)
-            elif action == "3":
-                break  
+                side_lenghtA = float(input("Enter the side A lenght: "))
+                side_lenghtB = float(input("Enter the side B lenght: "))
+                rectangle = Rectangle(side_lenghtA, side_lenghtB)
+                print("Area of the rectangle is: ", rectangle.calculate_area())
             else:
-                print("Wrong action. Try again!")
-                
-    elif choose == '4':
-        break  
+                print ("Error. Try again!")
+                continue
+            break
+    elif choose == "3":
+        while True:
+            print("1. Circuit calculations")
+            print("2. Field calculations")
+            action = input("Chose your action: ")
+            if action == "1":
+                side_lenght = float(input("Enter the side lenght: "))
+                triangle = Triangle_equilateral(side_lenght)
+                print("Perimeter of triangle equilateral is: ", triangle.calculate_permiter())
+            elif action == "2":
+                side_lenght = float(input("Enter the side lenght: "))
+                triangle = Triangle_equilateral(side_lenght)
+                print("Area of triangle equilateral is: ", triangle.calculate_area())
+            else:
+                print("Error! Try again!")
+                continue
+            break
+    elif choose =="4":
+        break
     else:
-        print("Wrong action. Try again!")
+        print("Error!Try again!")
