e39a8967320062720c597af4437cc3bc6b35ad2e
Sboov <patryk.padol@gmail.com>
2024-06-11 00:28:26 +0200

    0.3v
    + added  Circle
    + bug fixes

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ class Square:
             raise ValueError("Side lenght must be a positive number")
         self.side_lenght = side_lenght
     
-    def calculate_permiter(self):
+    def calculate_perimeter(self):
         return self.side_lenght *4
     def calculate_area(self):
-        return self.side_lenght^2
+        return self.side_lenght**2
 class Rectangle:
     def __init__(self, side_lenghtA, side_lenghtB):
         if side_lenghtA <= 0 or side_lenghtB <= 0:
@@ -16,7 +16,7 @@ class Rectangle:
         self.side_lenghtA = side_lenghtA
         self.side_lenghtB = side_lenghtB
 
-    def calculate_permiter(self):
+    def calculate_perimeter(self):
         return (self.side_lenghtB *2 ) +(self.side_lenghtA * 2)
     def calculate_area(self):
         return self.side_lenghtA * self.side_lenghtB
@@ -25,17 +25,27 @@ class Triangle_equilateral:
         if side_lenght <=0:
             raise ValueError("Side lenght must be a positive number")
         self.side_lenght = side_lenght
-    def calculate_permiter(self):
+    def calculate_perimeter(self):
         return self.side_lenght *3
     def calculate_area(self):
         return (math.sqrt(3) / 2) * self.side_lenght
+class Circle:
+    def __init__ (self, radius,):
+        if radius <=0:
+            raise ValueError("Radius must be a pistive number")
+        self.radius = radius
+    def calculate_perimeter(self):
+        return 2*math.pi*self.radius
+    def calculate_area(self):
+        return math.pi*self.radius**2
 
+        
 while True:
     print("Main menu: ")
     print("1. Square")
     print("2. Rectangle")
     print("3. Equilateral triangle")
-    print("4. Exit")
+    print("4. Circle")
     choose = input("Choose your Action: ")
     if choose == "1":
         while True:
@@ -45,7 +55,7 @@ while True:
             if action == "1":
                 side_lenght = float(input("Enter the side lenght: "))
                 square = Square(side_lenght)
-                print("Perimeter of the square:", square.calculate_permiter())
+                print("Perimeter of the square:", square.calculate_perimeter())
             elif action == "2":
                 side_lenght = float(input("Enter the side lenght: "))
                 square = Square(side_lenght)
@@ -63,7 +73,7 @@ while True:
                 side_lenghtA = float(input("Enter the side A lenght: "))
                 side_lenghtB = float(input("Enter the side B lenght: "))
                 rectangle = Rectangle(side_lenghtA, side_lenghtB)
-                print ("Permieter of rectangle is: ", rectangle.calculate_permiter())
+                print ("Permieter of rectangle is: ", rectangle.calculate_perimeter())
             elif action == "2":
                 side_lenghtA = float(input("Enter the side A lenght: "))
                 side_lenghtB = float(input("Enter the side B lenght: "))
@@ -91,6 +101,21 @@ while True:
                 continue
             break
     elif choose =="4":
-        break
+        while True:
+            print("1. Circuit calculations")
+            print("2. Field calculations")
+            action = input("Choose your action: ")
+            if action == "1":
+                radius = float(input("Type radius: "))
+                circle = Circle(radius)
+                print("Permiter of circle is: ", circle.calculate_perimiter())
+            elif action == "2":
+                radius = float(input("Type your radius:" ))
+                circle = Circle(radius)
+                print("Area of circle is: ", circle.calculate_area())
+            else:
+                print("Error! Try again!")
+                continue
+            break
     else:
         print("Error!Try again!")
